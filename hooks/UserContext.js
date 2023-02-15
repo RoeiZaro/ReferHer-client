d0ad9c286b9c428b362fe0ab0159c72a91e4b36c
RoeiZaro <119501395+RoeiZaro@users.noreply.github.com>
2023-02-15 20:08:17 +0200

    Update UserContext.js

--- hooks/UserContext.js
+++ hooks/UserContext.js
@@ -1,45 +1,23 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 import { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
-const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+const UserProvider = ({ children, data }) => {
+  const navigate = useNavigation();
+
+  const [user, setUser] = useState(data);
   const [token, setToken] = useState(null);
 
-  const login = async ({ username, password }) => {
-    try {
-      const res = await axios.post("http://localhost:8000/login", {
-        username,
-        password,
-      });
-      if (res.status === 201) {
-        alert("user not found");
-      }
-      if (res.status === 200) {
-        storeData(res.data.token);
-        setUser(res.data.user);
-        setToken(res.data.token);
-        setTimeout(() => {
-          navigate("/home");
-        }, 900);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const storeData = async (value) => {
-    try {
-      await AsyncStorage.setItem("token", value);
-    } catch (e) {
-      console.log(e);
-    }
-  };
   return (
     <UserContext.Provider
       value={{
         user,
-        login,
+        setUser,
+        token,
+        setToken,
       }}
     >
       {children}
