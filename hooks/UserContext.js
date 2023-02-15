c6130340f002a8ecd83d28e9ca7addf7ee493c4b
RoeiZaro <119501395+RoeiZaro@users.noreply.github.com>
2023-02-15 11:11:09 +0200

    Update UserContext.js

--- hooks/UserContext.js
+++ hooks/UserContext.js
@@ -1,1 +1,18 @@
-a
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const user = "roei";
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
