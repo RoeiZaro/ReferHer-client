3e93009eaf0fb6b6743e4475501a5db87c1a9883
RoeiZaro <119501395+RoeiZaro@users.noreply.github.com>
2023-02-15 12:21:25 +0200

    Update UserContext.js

--- hooks/UserContext.js
+++ hooks/UserContext.js
@@ -3,11 +3,43 @@ import { createContext, useState } from "react";
 export const UserContext = createContext();
 
 const UserProvider = ({ children }) => {
-  const user = "roei";
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+
+  const login = async ({ username, password }) => {
+    try {
+      const res = await axios.post("http://localhost:8000/login", {
+        username,
+        password,
+      });
+      if (res.status === 201) {
+        alert("user not found");
+      }
+      if (res.status === 200) {
+        storeData(res.data.token);
+        setUser(res.data.user);
+        setToken(res.data.token);
+        setTimeout(() => {
+          navigate("/home");
+        }, 900);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const storeData = async (value) => {
+    try {
+      await AsyncStorage.setItem("token", value);
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <UserContext.Provider
       value={{
         user,
+        login,
       }}
     >
       {children}
